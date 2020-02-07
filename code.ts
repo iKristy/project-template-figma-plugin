adf8b7e0bd7cbdeec1934fc5274b5b98b1c525b2
Rafael Conde <rafahari@me.com>
2020-02-07 14:41:04 +0100

    Add an update command, that only updates the cover

--- code.ts
+++ code.ts
@@ -7,44 +7,7 @@ async function main() {
 
 main().then(() => {
 
-  let oldTitleArray = []
-
   figma.ui.onmessage = msg => {
-
-    figma.root.children.forEach(page => {
-
-      if(page.name === msg.titletext) {
-        
-        page.children.forEach(child => {
-
-          if (child.type === "FRAME") {
-            child.children.forEach(newChild => {
-              if (newChild.type === "TEXT" && newChild.name === "Title") {
-                let oldTitle = newChild.characters
-                oldTitleArray.push(oldTitle)
-              }
-            })
-          }
-        })
-      }
-    })
-  }
-
-  figma.ui.onmessage = msg => {
-
-    figma.root.children.forEach(page => {
-      if(page.name === msg.titletext) {
-        
-        page.children.forEach(child => {
-          child.remove()
-        })
-        
-        page.name = "sdc9s098c0s98"
-      }
-      if(page.name === "—————————") {
-        page.remove()
-      }
-    })
 
     if (msg.type === 'create-template') {
   
@@ -110,17 +73,7 @@ main().then(() => {
       async function picker(hex1,hex2,hex3,newLabel) {
         page.backgrounds = [{type: 'SOLID', color: {r: hex1 / 255, g: hex2 / 255, b: hex3 / 255}}]
         frame.backgrounds = [{type: 'SOLID', visible: false, color: {r: hex1 / 255, g: hex2 / 255, b: hex3 / 255}}]
-
-        if (pageTitle.length === 0) {
-
-          if (oldTitleArray.length >= 1) {
-            text.characters = oldTitleArray.toString()
-          } else {
-            text.characters = "Title"
-          }  
-        } else {
-          text.characters = pageTitle
-        }
+        text.characters = pageTitle
         label.characters = newLabel
         url.characters = projectURL
       }
@@ -147,12 +100,82 @@ main().then(() => {
       figma.root.insertChild(5, pageBreak);
     }
 
-    figma.root.children.forEach(page => {
+    if (msg.type === 'update-template') {
+  
+      let pageTitle = msg.titletext
+      let projectURL = msg.urlText
+      let bgColor = msg.colorValue
+      let frame = figma.createFrame()
+      let page = figma.createPage()
+      let text = figma.createText()
+      let label = figma.createText()
+      let url = figma.createText()
+   
+      page.appendChild(frame)
+      frame.appendChild(text)
+      frame.appendChild(label)
+      frame.appendChild(url)
+      frame.resize(620,320)
 
-      if (page.name === "sdc9s098c0s98") {
-        page.remove()
+      page.name = pageTitle
+      label.name = "Label"
+      frame.name = "Thumb"
+      text.name = "Title"
+      url.name = "Project URL"
+
+      text.x = 0
+      text.y = 20
+      text.fontSize = 46
+      text.textAlignVertical = "CENTER"
+      text.textAutoResize = "NONE"
+      text.resize(620,320)
+      text.fontName = { family: "Roboto", style: "Bold" }
+      text.fills = [{type: 'SOLID', color: {r: 255 / 255, g: 255 / 255, b: 255 / 255}}]
+
+      label.y = 0
+      label.x = 0
+      label.fontSize = 32
+      label.textAlignHorizontal ="LEFT"
+      label.textAutoResize = "WIDTH_AND_HEIGHT"
+      label.textAlignVertical = "TOP"
+      label.fontName = { family: "Roboto", style: "Regular" }
+      label.fills = [{type: 'SOLID', color: {r: 255 / 255, g: 255 / 255, b: 255 / 255}}]
+
+      url.y = 187
+      url.x = 0
+      url.fontSize = 22
+      url.textAlignHorizontal ="LEFT"
+      url.textAutoResize = "WIDTH_AND_HEIGHT"
+      url.textAlignVertical = "TOP"
+      url.fontName = { family: "Roboto", style: "Regular" }
+      url.fills = [{type: 'SOLID', color: {r: 255 / 255, g: 255 / 255, b: 255 / 255}}]
+      url.opacity = 0.5
+
+      async function picker2(hex1,hex2,hex3,newLabel) {
+        page.backgrounds = [{type: 'SOLID', color: {r: hex1 / 255, g: hex2 / 255, b: hex3 / 255}}]
+        frame.backgrounds = [{type: 'SOLID', visible: false, color: {r: hex1 / 255, g: hex2 / 255, b: hex3 / 255}}]
+        text.characters = pageTitle
+        label.characters = newLabel
+        url.characters = projectURL
       }
-    })
+
+      if (bgColor === "1") {
+        picker2(0,122,229,"Work in Progress")
+      }
+  
+      if (bgColor === "2") {
+        picker2(0,173,159,"Ready to Review")
+      }
+  
+      if (bgColor === "3") {
+        picker2(155,0,245,"Live")
+      }
+
+      figma.currentPage = page
+      figma.viewport.zoom = 1
+      figma.root.insertChild(0, page);
+      figma.root.children[1].remove()
+    }
  
     figma.closePlugin();
   
